d51a395c849996840907562879ba7a653f34b15b
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2024-05-02 12:33:16 +0300

    fix: add normal styles names

--- configs/rules/style_modules.js
+++ configs/rules/style_modules.js
@@ -3,6 +3,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = (prod = false) => {
     return {
     test: /\.module\.(scss|css)$/,
-    use: [prod ? MiniCssExtractPlugin.loader : 'style-loader', { loader: 'css-loader', options: { modules: true } }, 'sass-loader'],
+    use: [prod ? MiniCssExtractPlugin.loader : 'style-loader', { loader: 'css-loader', options: { modules: {
+        localIdentName:'[local]__[hash:base64:5]',
+    } } }, 'sass-loader'],
 }
 }
